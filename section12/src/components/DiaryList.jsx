5aed43dab2bbc8df9d11d65b4a5f8801d5b02525
geonwoo05 <rlarjsdn531@gmail.com>
2024-08-25 14:34:35 +0900

    테스트

--- section12/src/components/DiaryList.jsx
+++ section12/src/components/DiaryList.jsx
@@ -1,23 +1,23 @@
-import "./DiaryList.css"
-import Button from "./Button";
-import DiaryItem from "./DiaryItem";
+import './DiaryList.css';
+import Button from './Button';
+import DiaryItem from './DiaryItem';
 
-
+// 테스트
 const DiaryList = () => {
     return (
         <div className="DiaryList">
             <div className="menu_bar">
                 <select>
-                    <option value={"latest"}>최신순</option>
-                    <option value={"ordest"}>오래된 순</option>
+                    <option value={'latest'}>최신순</option>
+                    <option value={'ordest'}>오래된 순</option>
                 </select>
-                <Button text={"새 일기 쓰기"} type={"POSITIVE"}/>
+                <Button text={'새 일기 쓰기'} type={'POSITIVE'} />
             </div>
             <div className="list_wrapper">
                 <DiaryItem />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default DiaryList;
